8d7845f914a12c46cfb802d318b541b9479e7532
xcash <paolo.casciello@gmail.com>
2014-07-24 17:18:55 +0200

    updated dev.py - now runs by ./dev.py <run|external>

--- dev.py
+++ dev.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python
 import sys
 
+from flask.ext.script import Manager
+
 from handler import app
 
-if __name__ == '__main__':
+manager = Manager(app)
+
+
+@manager.command
+def run():
 	print app.url_map
-	if 'external' in sys.argv[1:]:
-		app.run(host='0.0.0.0')
-	else:
-		app.run()
+	app.run()
+
+@manager.command
+def runserver():
+	run()
+
+@manager.command
+def external():
+	app.run(host='0.0.0.0')
+	
+
+if __name__ == '__main__':
+	manager.run()
